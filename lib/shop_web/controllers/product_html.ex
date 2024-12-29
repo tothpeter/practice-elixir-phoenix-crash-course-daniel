f81c7b7ad8c5dac059fad4d67e8c508a3ddfaa2b
Peter Toth <tothpeter08@gmail.com>
2024-12-29 19:24:00 +0700

    #3: Request life-cycle

---
+++ lib/shop_web/controllers/product_html.ex
@@ -0,0 +1,11 @@
+defmodule ShopWeb.ProductHTML do
+  use ShopWeb, :html
+
+  embed_templates "product_html/*"
+
+  # def index(assigns) do
+  #   ~H"""
+  #     <h1>Asd</h1>
+  #   """
+  # end
+end
